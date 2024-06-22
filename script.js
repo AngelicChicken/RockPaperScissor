aee6d6b0b6eb6aea61d5b0523f52dee4e49a7fcc
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-06-23 03:16:39 +0700

    made function that generate random choice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello")
+// get 3 random output
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+
+// link 3 random output to rock paper scissor
+function getComputerChoice(){
+    let computerChoice = getRandomInt(3);
+    if(computerChoice === 0){
+        return "paper"
+    } if(computerChoice === 1){
+        return "rock"
+    } else {
+        return "scissor"
+    }
+}
+
+
+// get user choice
+// make rock lose to paper
+// make paper lose to scissor
+// make scisor lose to rock
+// compare who win
